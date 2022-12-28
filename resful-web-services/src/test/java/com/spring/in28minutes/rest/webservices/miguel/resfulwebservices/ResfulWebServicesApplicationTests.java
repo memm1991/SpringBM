784564814cbc3ac740a84778862ab93c281c12ce
Miguel <miguel.mejail@kinandcarta.com>
2022-12-28 15:46:33 -0300

    firstr commit

---
+++ resful-web-services/src/test/java/com/spring/in28minutes/rest/webservices/miguel/resfulwebservices/ResfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.in28minutes.rest.webservices.miguel.resfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
